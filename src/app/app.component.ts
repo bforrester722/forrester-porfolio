f4a0b288b6d5b82d89a0e88aeb0c88f9eac5f894
bforrester722 <b.forrester@live.com>
2022-11-29 16:29:10 -0600

    updates dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, ViewChild, OnDestroy, ViewChildren, QueryList  } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChildren,
+  QueryList,
+} from '@angular/core';
 import { WindowComponent } from './components/window/window.component';
 // Services
 import { DataService } from './core/data.service';
@@ -12,129 +18,143 @@ import { wait } from './helpers/helper';
 @Component({
   selector: 'app-component',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-
-export class AppComponent implements OnInit, OnDestroy{
-
-  @ViewChildren(WindowComponent) private childrenFolder!: QueryList<WindowComponent>;
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChildren(WindowComponent)
+  private childrenFolder!: QueryList<WindowComponent>;
 
   // varibles
   screenHeight: number = 0;
   // subscriptions
-  subscription:           Subscription;
-  openSubscription:       Subscription;
+  subscription: Subscription;
+  openSubscription: Subscription;
   animationsSubscription: Subscription;
   // interfaces
-  icons:        IIcons[] = [];
-  animations:   IAnimation[] = [];
-  openFiles:    Array<IOpenFile> = [];
-  portfolio:    IPortfolio[] = [];
- 
-  constructor(private firestoreService: FirestoreService, 
-              private projectService: ProjectService,
-              private dataService: DataService) {
+  icons: IIcons[] = [];
+  animations: IAnimation[] = [];
+  openFiles: Array<IOpenFile> = [];
+  portfolio: IPortfolio[] = [];
 
+  constructor(
+    private firestoreService: FirestoreService,
+    private projectService: ProjectService,
+    private dataService: DataService
+  ) {
     // ran when arrow in pic-view to change project in window
-    this.subscription = this.projectService.getProject().subscribe(project => {
-      if (project) {
-        this.projectChanged(project);
-      } 
-    });
+    this.subscription = this.projectService
+      .getProject()
+      .subscribe((project) => {
+        if (project) {
+          this.projectChanged(project);
+        }
+      });
 
-    // ran when file selected from folder 
-    this.openSubscription = this.projectService.openProject().subscribe(async project => {
-      if (project) {
-        await wait(20);
-        this.openFile(project);
-      } 
-    });
+    // ran when file selected from folder
+    this.openSubscription = this.projectService
+      .openProject()
+      .subscribe(async (project) => {
+        if (project) {
+          await wait(20);
+          this.openFile(project);
+        }
+      });
 
     // gets animation data from animations.json, sorts by name, and adds index
-    this.animationsSubscription = this.dataService.getAnimations().subscribe(animations => {
-      if (animations) {
-        const sorted = animations.sort((a, b) => (a.name > b.name) ? 1 : -1);
-        this.animations = sorted.map((animation, index) => {
-          return {...animation, index};
-        })
-      }
-     
-    });
-
+    this.animationsSubscription = this.dataService
+      .getAnimations()
+      .subscribe((animations) => {
+        if (animations) {
+          const sorted = animations.sort((a, b) => (a.name > b.name ? 1 : -1));
+          this.animations = sorted.map((animation, index) => {
+            return { ...animation, index };
+          });
+        }
+      });
   }
 
-
- ngOnInit() {
-  // fixes address bar on phones making 100vh not work correctly 
-  this.screenHeight = window.innerHeight;
-  // uses firestoreSevice to get icons
-  this.firestoreService.getCollection('icons').subscribe(icons => {
-    const sorted = icons.sort((a, b) => (a.name > b.name) ? 1 : -1);
-    this.icons = sorted.map((animation, index) => {
-      return {...animation, index, viewer: 'pic'};
-    })
-  });
-  // used to get portfolio json
-  this.dataService.getPortfolio().subscribe((portfolio: any) => {
-    this.portfolio = portfolio;
+  ngOnInit() {
+    // fixes address bar on phones making 100vh not work correctly
+    this.screenHeight = window.innerHeight;
+    // uses firestoreSevice to get icons
+    this.firestoreService.getCollection('icons').subscribe((icons) => {
+      const sorted = icons.sort((a, b) => (a.name > b.name ? 1 : -1));
+      this.icons = sorted.map((animation, index) => {
+        return { ...animation, index, viewer: 'pic' };
+      });
+    });
+    // used to get portfolio json
+    this.dataService.getPortfolio().subscribe((portfolio: any) => {
+      this.portfolio = portfolio;
     });
   }
 
-  // fixes address bar on phones making 100vh not work correctly 
-  onResize(event:any) {
-    const {innerHeight} = event.target;
+  // fixes address bar on phones making 100vh not work correctly
+  onResize(event: any) {
+    const { innerHeight } = event.target;
     this.screenHeight = innerHeight;
   }
 
   // ran when left or right clicked from pic-view
   projectChanged(project: any) {
-    const {width, height, top, left} = this.childrenFolder.toArray()[project.openIndex]
-    const {index, openIndex} = project;
-    const type  = project.src ? this.icons : this.animations;
+    const { width, height, top, left } =
+      this.childrenFolder.toArray()[project.openIndex];
+    const { index, openIndex } = project;
+    const type = project.src ? this.icons : this.animations;
     let newIndex = index + 1 > type.length ? 0 : index;
     newIndex = newIndex === -1 ? type.length - 1 : newIndex;
     // TODO:  fix doesn't smell right
-    this.openFiles.splice(openIndex, 1, {viewer: 'na', ...type[newIndex], lastClicked: true, openIndex, width, height, top, left});
-
+    this.openFiles.splice(openIndex, 1, {
+      viewer: 'na',
+      ...type[newIndex],
+      lastClicked: true,
+      openIndex,
+      width,
+      height,
+      top,
+      left,
+    });
   }
 
-	// emitted from taskbar to maximize window
-	maximizeWindow(project: any) {
-    this.childrenFolder.toArray()[project.openIndex].maximize()
-    this.setLastClicked(project)
-	}
+  // emitted from taskbar to maximize window
+  maximizeWindow(project: any) {
+    this.childrenFolder.toArray()[project.openIndex].maximize();
+    this.setLastClicked(project);
+  }
 
   // For having windows open with random dimensions and placement
   getRandom() {
     // if small screen open full width and height
-    if ( window.innerWidth < 700) {
+    if (window.innerWidth < 700) {
       const height = window.innerHeight - 40;
       const width = window.innerWidth;
-      return {height, width, left: 0, top: 0}
+      return { height, width, left: 0, top: 0 };
     }
     const plus = window.innerWidth / 4;
     const height = Math.floor(Math.random() * 200) + plus;
     const width = height + 100;
     const top = Math.floor(Math.random() * 200) + 50;
-    const left = Math.floor(Math.random() * plus) + (plus / 12);
-    return {height, width, top, left};
+    const left = Math.floor(Math.random() * plus) + plus / 12;
+    return { height, width, top, left };
   }
 
   // push selected file to openFiles and add index for keeping track of file
   openFile(data: any) {
-    if (!data) { return; }
-    const {length} = this.openFiles;
+    if (!data) {
+      return;
+    }
+    const { length } = this.openFiles;
     data.openIndex = length;
-    const updated  = {...data, ...this.getRandom()};
-    this.openFiles.push(updated); 
+    const updated = { ...data, ...this.getRandom() };
+    this.openFiles.push(updated);
     this.setLastClicked(updated);
   }
 
   // sets lastClicked for making last selected viewer on top
   // TODO other previous windows need to be on top in order of last viewed
   setLastClicked(data: any) {
-    this.openFiles.map(file => {
-      if(file.openIndex === data.openIndex) {
+    this.openFiles.map((file) => {
+      if (file.openIndex === data.openIndex) {
         file.lastClicked = true;
         return;
       }
@@ -142,39 +162,48 @@ export class AppComponent implements OnInit, OnDestroy{
     });
   }
 
-  // removes file from openFiles when window closed 
+  // removes file from openFiles when window closed
   windowClosed(project: any) {
     this.openFiles.splice(project.openIndex, 1);
     this.openFiles.map((file, index) => {
-      file.openIndex = index
+      file.openIndex = index;
       return file;
-    })
+    });
   }
 
   // opens folder and passes in data
   openDesktopFolder(item: any) {
-    const {name} = item;
-    const newItem = {...item, ...this.getRandom()};
-    if (newItem.viewer === "doc") {
+    const { name } = item;
+    const newItem = { ...item, ...this.getRandom() };
+    if (newItem.viewer === 'doc') {
       newItem.openIndex = this.openFiles.length;
       this.openFiles.push(newItem);
       this.setLastClicked(newItem);
       return;
     }
     const type = () => {
-      if (name === "animations") {
+      if (name === 'animations') {
         return [...this.animations];
       }
-      if (name === "icons") {
-        return [...this.icons]
-
+      if (name === 'icons') {
+        return [...this.icons];
       }
       return [...this.portfolio];
-    }
+    };
     const items = type();
-    const {height, width, top, left} = this.getRandom();
+    const { height, width, top, left } = this.getRandom();
     // TODO:  fix doesn't smell right
-    const builtItem = {lastClicked: true, items, viewer: item.viewer, name: name, openIndex: this.openFiles.length, height, width, top, left};
+    const builtItem = {
+      lastClicked: true,
+      items,
+      viewer: item.viewer,
+      name: name,
+      openIndex: this.openFiles.length,
+      height,
+      width,
+      top,
+      left,
+    };
     this.openFiles.push(builtItem);
     this.setLastClicked(builtItem);
   }
@@ -183,6 +212,4 @@ export class AppComponent implements OnInit, OnDestroy{
     // prevent memory leak when component destroyed
     this.subscription.unsubscribe();
   }
-
 }
-
--- src/app/components/window/window.component.ts
+++ src/app/components/window/window.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, Output, EventEmitter, AfterViewInit, HostListener, Renderer2  } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+  HostListener,
+  Renderer2,
+} from '@angular/core';
 import { wait } from '../../helpers/helper';
 
 const enum Status {
   OFF = 0,
   RESIZE = 1,
-  MOVE = 2
+  MOVE = 2,
 }
-
 
 @Component({
   selector: 'app-window',
   templateUrl: './window.component.html',
-  styleUrls: ['./window.component.sass']
+  styleUrls: ['./window.component.sass'],
 })
-
 export class WindowComponent implements AfterViewInit {
   @Input() width: number = 0;
   @Input() height: number = 0;
@@ -27,7 +36,7 @@ export class WindowComponent implements AfterViewInit {
   private cachedWidth: any;
   private cachedHeight: any;
   private cachedLeft: any;
-  public mouse: any
+  public mouse: any;
   public status: Status = Status.OFF;
   private mouseClick: any;
   @Output() closed: EventEmitter<boolean> = new EventEmitter<boolean>();
@@ -40,39 +49,45 @@ export class WindowComponent implements AfterViewInit {
     return this._project;
   }
 
-  set project(value: any ) {
+  set project(value: any) {
     if (value) {
       this._project = value;
       this.loadBackground();
-    	return;
+      return;
     }
   }
 
   // sets backgroundColor
   async loadBackground() {
-    await wait(10);  
-    const {bg} = this.project;
-    if (!this.box) { return; }
-     this.box.nativeElement.style.backgroundColor = bg ? bg : 'white';
+    await wait(10);
+    const { bg } = this.project;
+    if (!this.box) {
+      return;
+    }
+    this.box.nativeElement.style.backgroundColor = bg ? bg : 'white';
   }
-  
+
   constructor(private renderer: Renderer2) {}
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.loadBox();
     this.loadContainer();
   }
 
   // sets window position
-  private loadBox(){
-  	if(!this.box) { return; }
-    const {left, top} = this.box.nativeElement.getBoundingClientRect();
-    this.boxPosition = {left, top};
+  private loadBox() {
+    if (!this.box) {
+      return;
+    }
+    const { left, top } = this.box.nativeElement.getBoundingClientRect();
+    this.boxPosition = { left, top };
   }
 
   // limits where you can move window on screen
-  private loadContainer(){
-  	if(!this.box) { return; }
+  private loadContainer() {
+    if (!this.box) {
+      return;
+    }
     const left = this.boxPosition.left - this.left - 128;
     const top = this.boxPosition.top - this.top - 32;
     const right = left + window.innerWidth + 256;
@@ -81,21 +96,29 @@ export class WindowComponent implements AfterViewInit {
   }
 
   // sets status based mouse on moving or resizing window
-  setStatus(event: MouseEvent, status: number){
-     this.box.nativeElement.style.transition = 'all .25s';
-     if (status) {
-      this.mouseClick = { x: event.clientX, y: event.clientY, left: this.left, top: this.top };
+  setStatus(event: MouseEvent, status: number) {
+    this.box.nativeElement.style.transition = 'all .25s';
+    if (status) {
+      this.mouseClick = {
+        x: event.clientX,
+        y: event.clientY,
+        left: this.left,
+        top: this.top,
+      };
       // adds listener to track mouse for move or resize
-      this.mouseMoveListener = this.renderer.listen('document', 'mousemove', e => {
-        this.mouse = { x: e.clientX, y: e.clientY };
-        if (this.status === 1) {
-          this.resize();
-          return
+      this.mouseMoveListener = this.renderer.listen(
+        'document',
+        'mousemove',
+        (e) => {
+          this.mouse = { x: e.clientX, y: e.clientY };
+          if (this.status === 1) {
+            this.resize();
+            return;
+          }
+          this.move();
         }
-        this.move();
-      });
-    }
-    else {
+      );
+    } else {
       if (this.mouseMoveListener) {
         this.mouseMoveListener();
       }
@@ -108,117 +131,133 @@ export class WindowComponent implements AfterViewInit {
   }
 
   // sets status based touch on moving or resizing window
-  setTouchStatus(event: TouchEvent, status: number){
-   	this.box.nativeElement.style.transition = 'all .25s';
-    if(status) {
-      const touch = event.targetTouches[0]
-      this.mouseClick = { x: touch.clientX, y: touch.clientY, left: this.left, top: this.top };
-      this.touchMoveListener = this.renderer.listen('document', 'touchmove', e => {
-        const touch = e.targetTouches[0];
-        this.mouse = { x: touch.clientX, y: touch.clientY };
-        if (this.status === 1) {
-          this.resize();
-          return
+  setTouchStatus(event: TouchEvent, status: number) {
+    this.box.nativeElement.style.transition = 'all .25s';
+    if (status) {
+      const touch = event.targetTouches[0];
+      this.mouseClick = {
+        x: touch.clientX,
+        y: touch.clientY,
+        left: this.left,
+        top: this.top,
+      };
+      this.touchMoveListener = this.renderer.listen(
+        'document',
+        'touchmove',
+        (e) => {
+          const touch = e.targetTouches[0];
+          this.mouse = { x: touch.clientX, y: touch.clientY };
+          if (this.status === 1) {
+            this.resize();
+            return;
+          }
+          this.move();
         }
-        this.move();
-      });
-    } 
-    else {
-       if (this.touchMoveListener) {
+      );
+    } else {
+      if (this.touchMoveListener) {
         this.touchMoveListener();
       }
- 
       this.loadBox();
     }
     this.status = status;
   }
 
-
   // re-adds transition on mouse up
-  addTransition(){
- 		this.box.nativeElement.style.transition = 'all .25s';
+  addTransition() {
+    this.box.nativeElement.style.transition = 'all .25s';
   }
 
   // resizes window
-  private resize(){
-  	this.box.nativeElement.style.transition = 'all 0s';
+  private resize() {
+    this.box.nativeElement.style.transition = 'all 0s';
     // if(this.resizeCondMeet()){
-      this.width = Number(this.mouse.x > this.boxPosition.left) ? this.mouse.x - this.boxPosition.left : 0;
-      this.height = Number(this.mouse.y > this.boxPosition.top) ? this.mouse.y - this.boxPosition.top : 0;
+    this.width = Number(this.mouse.x > this.boxPosition.left)
+      ? this.mouse.x - this.boxPosition.left
+      : 0;
+    this.height = Number(this.mouse.y > this.boxPosition.top)
+      ? this.mouse.y - this.boxPosition.top
+      : 0;
     // }
   }
 
   // contains resize of window to screen
   // TODO: choppy find a better way to do this
-  private resizeCondMeet(){
-    return (this.mouse.x < this.containerPos.right - 128 && this.mouse.y < this.containerPos.bottom);
+  private resizeCondMeet() {
+    return (
+      this.mouse.x < this.containerPos.right - 128 &&
+      this.mouse.y < this.containerPos.bottom
+    );
   }
 
   // moves window
-  private move(){
-
-  	this.box.nativeElement.style.transition = 'all 0s';
+  private move() {
+    this.box.nativeElement.style.transition = 'all 0s';
     // if(this.moveCondMeet()){
-      this.left = this.mouseClick.left + (this.mouse.x - this.mouseClick.x);
-      this.top = this.mouseClick.top + (this.mouse.y - this.mouseClick.y);
+    this.left = this.mouseClick.left + (this.mouse.x - this.mouseClick.x);
+    this.top = this.mouseClick.top + (this.mouse.y - this.mouseClick.y);
     // }
   }
 
   // contains movement of window to screen
   // TODO: choppy find a better way to do this
-  private moveCondMeet(){
-    const offsetLeft = this.mouseClick.x - this.boxPosition.left; 
-    const offsetRight = this.width - offsetLeft; 
+  private moveCondMeet() {
+    const offsetLeft = this.mouseClick.x - this.boxPosition.left;
+    const offsetRight = this.width - offsetLeft;
     const offsetTop = this.mouseClick.y - this.boxPosition.top;
     const offsetBottom = this.height - offsetTop;
     return (
-      this.mouse.x > this.containerPos.left + offsetLeft && 
+      this.mouse.x > this.containerPos.left + offsetLeft &&
       this.mouse.x < this.containerPos.right - offsetRight &&
       this.mouse.y > this.containerPos.top + offsetTop &&
       this.mouse.y < this.containerPos.bottom - offsetBottom
-      );
+    );
   }
 
   // minimizes window
   minimizeClicked() {
-  	const {left, top} = this.box.nativeElement.getBoundingClientRect();
-  	this.cachedMinimizedTop = top;
-  	this.box.nativeElement.style.transform = `translate3d(${left}px, 100vh, 0px)`;
+    const { left, top } = this.box.nativeElement.getBoundingClientRect();
+    this.cachedMinimizedTop = top;
+    this.box.nativeElement.style.transform = `translate3d(${left}px, 100vh, 0px)`;
   }
 
   // restore and maximize window
-	maximizeClicked() {
-
-		const {innerHeight, innerWidth} = window;
+  maximizeClicked() {
+    const { innerHeight, innerWidth } = window;
     // if not maximized cache dimensions for when restored
-		if (this.width !== innerWidth && this.height !== innerHeight) {
-      this.cachedWidth   = this.width;
-			this.cachedHeight  = this.height;
-			this.cachedLeft    = this.left;
-			this.cachedTop     = this.top;
-		}
+    if (this.width !== innerWidth && this.height !== innerHeight) {
+      this.cachedWidth = this.width;
+      this.cachedHeight = this.height;
+      this.cachedLeft = this.left;
+      this.cachedTop = this.top;
+    }
     // if already full screen restore to cached size and position
-		if (this.width === innerWidth && this.height === innerHeight - 40) {
+    if (this.width === innerWidth && this.height === innerHeight - 40) {
       if (this.cachedWidth) {
-        this.updateSize(this.cachedWidth, this.cachedHeight, this.cachedLeft, this.cachedTop); 
+        this.updateSize(
+          this.cachedWidth,
+          this.cachedHeight,
+          this.cachedLeft,
+          this.cachedTop
+        );
         return;
       }
       // for phone size opens at full width and height so no cached
       this.updateSize(this.width / 2, this.height / 2, 32, 16);
       return;
-		}
+    }
     // otherwise make window max width and height
     this.updateSize(innerWidth, innerHeight - 40, 0, 0);
-	}
+  }
 
   // sets dimensions and postion of window
   updateSize(width: number, height: number, left: number, top: number) {
-    const {innerHeight, innerWidth} = window;
+    const { innerHeight, innerWidth } = window;
     window.requestAnimationFrame(() => {
-      this.width  = width;
+      this.width = width;
       this.height = height;
-      this.left   = left;
-      this.top    = top;
+      this.left = left;
+      this.top = top;
     });
   }
 
@@ -228,13 +267,12 @@ export class WindowComponent implements AfterViewInit {
     this.closed.emit(this.project); //for taskbar
   }
 
-	// called from app-component to restore window if needed
+  // called from app-component to restore window if needed
   maximize() {
-  	const {left, top} = this.box.nativeElement.getBoundingClientRect();
-    if (top < window.innerHeight) { return; }
- 		this.box.nativeElement.style.transform = `translate3d(${left}px, ${this.cachedMinimizedTop}px, 0px)`;
-	}
-
-
-
+    const { left, top } = this.box.nativeElement.getBoundingClientRect();
+    if (top < window.innerHeight) {
+      return;
+    }
+    this.box.nativeElement.style.transform = `translate3d(${left}px, ${this.cachedMinimizedTop}px, 0px)`;
+  }
 }
